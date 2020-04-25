9a10bc545f80af1fd1453ca4be2526254faaf1c9
materialcoder <1028106985@qq.com>
2020-04-25 17:50:50 +0800

    complete component Button

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button>Default Button</Button>
+        <Button className="custom" onClick={(e) => {e.preventDefault();alert(123)}}>Default Button</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Primary Button</Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Button</Button>
-        <Button btnType={ButtonType.Link} href="https://www.baidu.com">Link Baidu</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com" target="_blank">Link Baidu</Button>
         <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled>Disabled Link</Button>
         <Button disabled>Disabled Button</Button>
       </header>
