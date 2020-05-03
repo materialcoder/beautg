323903f755b41bc3cafd8e0833488ba41a947495
Henry <henry@Mac.local>
2020-05-03 17:28:48 +0800

    fix warning

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useContext, useState } from 'react'
-import {CSSTransition} from 'react-transition-group'
+// import {CSSTransition} from 'react-transition-group'
 import classNames from 'classnames'
 import { MenuContext } from './menu'
 import { MenuItemProps } from './menuItem';
