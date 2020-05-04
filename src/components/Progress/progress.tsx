e61345f874c9f2052a000d7a66b9b254250bbbe8
Henry <henry@Mac.local>
2020-05-04 20:49:57 +0800

    component Progress storybook

--- src/components/Progress/progress.tsx
+++ src/components/Progress/progress.tsx
@@ -14,7 +14,7 @@ export interface ProgressProps {
   theme?: ThemeProps
 }
 
-const Progress: FC<ProgressProps> = (props) => {
+export const Progress: FC<ProgressProps> = (props) => {
   const {
     percent,
     strokeHeight,
