4cc91f0721a4336ff18c579e438038095781724b
Henry <henry@Mac.local>
2020-05-06 10:16:20 +0800

    clean app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,9 @@
 import React from 'react';
-import Button from './components/Button/button';
-import Alert, { AlertType } from './components/Alert/alert';
-import MenuItem from './components/Menu/menuItem';
-import Menu from './components/Menu/menu';
-import SubMenu from './components/Menu/subMenu';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import Icon from './components/Icon/icon'
-
-// fas 引入所有图标
-library.add(fas)
-
 function App() {
   return (
     <div className="App">
-      {/* <FontAwesomeIcon icon={faCoffee} size="lg"/> */}
-      <Icon theme="primary" icon="arrow-down" size="10x" />
       <header className="App-header">
-        <Button className="custom" onClick={(e) => {e.preventDefault();alert(123)}}>Default Button</Button>
-        <Button btnType='primary' size='lg'>Primary Button</Button>
-        <Button btnType='danger' size='sm'>Small Button</Button>
-        <Button btnType='link' href="https://www.baidu.com" target="_blank">Link Baidu</Button>
-        <Button btnType='link' href="https://www.baidu.com" disabled>Disabled Link</Button>
-        <Button disabled>Disabled Button</Button>
-        <Alert title="This is Default" onClose={() => {console.log('closed')}} />
-        <Alert title="This is Success" type="success" />
-        <Alert title="This is Warning" description="this is a long description" type="warning" />
-        <Alert closable={false} title="This is Danger" type="danger" />
-        <Menu defaultIndex='0' defaultOpenSubMenus={['2']}>
-          <MenuItem>cool link</MenuItem>
-          <MenuItem disabled>cool link 2</MenuItem>
-          <SubMenu title="dropdown">
-            <MenuItem>dropdown 1</MenuItem>
-            <MenuItem>dropdown 2</MenuItem>
-            <MenuItem>dropdown 3</MenuItem>
-          </SubMenu>
-        </Menu>
+        component
       </header>
     </div>
   );
