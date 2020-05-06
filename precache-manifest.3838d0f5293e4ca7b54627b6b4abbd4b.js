336bf726b70249d30322721ebd6493fa5e1fdd0d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-06 10:31:28 +0000

    Deploy materialcoder/beautg to github.com/materialcoder/beautg.git:gh-pages

---
+++ precache-manifest.3838d0f5293e4ca7b54627b6b4abbd4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba67f38e079ef2d4edcba81c68e0e6c3",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a62d0e79d35c2c54000a.bundle.js"
+  },
+  {
+    "url": "runtime~main.a62d0e79d35c2c54000a.bundle.js"
+  },
+  {
+    "revision": "4bed69b86d9db526435c",
+    "url": "static/css/main.db2582ff.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.a62d0e79d35c2c54000a.bundle.js"
+  },
+  {
+    "url": "vendors~main.a62d0e79d35c2c54000a.bundle.js.LICENSE.txt"
+  }
+]);
